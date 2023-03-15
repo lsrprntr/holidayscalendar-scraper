98409fd11d656a8fd26ab0aa720d547151411777
sssst <lsrprntr@gmail.com>
2023-03-15 14:43:46 +1100

    Update holi.py

--- holi.py
+++ holi.py
@@ -1,6 +1,10 @@
 
 import urllib.request, urllib.parse, urllib.error
-
+from bs4 import BeautifulSoup
 
 page = urllib.request.urlopen('https://www.holidayscalendar.com/categories/international/')
-print(page.read())
+#print(page.read())
+
+soup = BeautifulSoup(page, 'html.parser')
+
+print(soup.prettify())
