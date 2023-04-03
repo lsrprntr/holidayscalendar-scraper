09ea6a6fb759ae969a077fc93d7418c048650a1c
lsr <lsrprntr@gmail.com>
2023-04-03 21:59:04 +1000

    update

--- holi.py
+++ holi.py
@@ -1,10 +1,23 @@
-
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 
-page = urllib.request.urlopen('https://www.holidayscalendar.com/categories/international/')
+fyear = str(input("What year?: ____"))
+link = "https://www.holidayscalendar.com/categories/international-"+fyear+"/"
+
+#page request html vomit
+page = urllib.request.urlopen('https://www.holidayscalendar.com/categories/international-2023/')
 #print(page.read())
+
 
 soup = BeautifulSoup(page, 'html.parser')
 
-print(soup.prettify())
+#print(soup)
+
+#find table
+#tabel = soup.find('table')
+#print(tabel)
+
+
+for i in soup.find_all("tr"):
+    for k in i.find_all("td"):
+        print(k.text)
